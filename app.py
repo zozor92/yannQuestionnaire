a5f06da89f4c0df3e632d70cbd8e00468274abf4
Mael Costantini <zozor92@hotmail.fr>
2016-04-15 18:28:28 -0400

    new feature POST - /result
    Type of json in input :
    {
        "name":"Mael",
        "mail":"mael.costantini@gmail.com",
    
    "matricule":"nohecMatricule",
        "questions":
        [
            {
    
    "question":"la premiere question",
            "response":"une des mauvaises
    reponses",
            "correct":"no"
            },
             {
    
    "question":"La deuxieme question",
            "response":"mauvaise
    reponse",
            "correct":"no"
            },
             {
    
    "question":"La troisieme question",
            "response":"la bonne
    reponse",
            "correct":"yes"
            }
        ]
    
    }
    
    This will create a xls sheet containing the name, matricule, mail and
    the questions+reponse gave from the student. The xls will be then send
    to a mail address.

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, jsonify, abort, make_response, request
 from models.models import Questions, Responses, db, Ref_type
 from random import randint
+from flask.ext.cors import CORS
+import xlwt, time, math, smtplib, os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 
 app = Flask(__name__)
+CORS(app)
 db.init_app(app)
 
 mathCatId = 1
@@ -48,14 +55,11 @@ def get_quiz():
         questionList.append(x)
     return jsonify(questions=questionList)
 
-# @app.route('/api/1/users', methods=['POST'])
-# def create_users():
-#    if not request.json or not '' in request.json:
-#        abort(400)
-#    user = Questions(request.json['name'])
-#    db.session.add(user)
-#    db.session.commit()
-#    return jsonify(users=[i.serialize for i in Questions.query.all()])
+@app.route('/api/1/result', methods=['POST'])
+def retrieveResults():
+    results =request.get_json();
+    createExcelSheet(results);
+    return jsonify(result="success")
 
 def questionbyID(id):
     return Questions.query.filter_by(questionID=id).first().serialize
@@ -70,13 +74,72 @@ def questionrepbyCat(cat):
         tableIDQuestCat.append(i.questionID)
     questionToPick = min(len(tableIDQuestCat), 5)
     b = []
-    for i in range(0, questionToPick):        b.append({'question':questionbyID(tableIDQuestCat[i]), 'responses' : responsesbyID(tableIDQuestCat[i])})
+    for i in range(0, questionToPick):b.append({'question':questionbyID(tableIDQuestCat[i]), 'responses' : responsesbyID(tableIDQuestCat[i])})
     return b
 
 
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
+
+def createExcelSheet(resultsToParse):
+    book = xlwt.Workbook()
+    filename = "results_" + time.strftime("%d%m%Y_%H%M") + "_" + resultsToParse["matricule"] + ".xls"
+    sh = book.add_sheet("results")
+    sh.write(0,0,time.strftime("%d/%m/%Y %H:%M"))
+    sh.write(1,0,resultsToParse["name"])
+    sh.write(2,0,resultsToParse["matricule"])
+    sh.write(3,0,resultsToParse["mail"])
+    sh.write(0,2,"Questions")
+    sh.write(0,3,"Response given")
+    sh.write(0,4,"Correct?")
+    questionNb=1
+    goodQuestion=0
+    for myquestion in resultsToParse["questions"]:
+        sh.write(questionNb,2,myquestion["question"])
+        sh.write(questionNb,3,myquestion["response"])
+        sh.write(questionNb,4,myquestion["correct"])
+        if myquestion["correct"] == 'yes':
+            goodQuestion+=1
+        questionNb+=1
+    sh.write(questionNb,4,math.ceil(goodQuestion/(questionNb-1)*100))
+    book.save(filename)
+    toaddr = "";
+    sendMailTo(toaddr, filename, resultsToParse["name"], resultsToParse["matricule"])
+    os.remove(filename)
+
+def sendMailTo(toaddr, filename, name, matricule):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    fromaddr = ""
+
+    msg = MIMEMultipart()
+
+    msg['From'] = fromaddr
+    msg['To'] = toaddr
+    msg['Subject'] = "Results of " + name + "_" + matricule
+
+    body = "In attachement, the result of " + name + "_" + matricule
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    attachment = open("D:\GitHub\yannQuestionnaire\yannQuestionnaire\\" + filename, "rb")
+
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+    msg.attach(part)
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(fromaddr, "password")
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
+
+
 
 
 if __name__ == '__main__':
